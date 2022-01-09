92e847acfbc43297608f3de276fa196bb4d23115
sollder1 <124xyZWER#>
2022-01-09 22:57:23 +0100

    Finish Tokenizer

--- src/main/java/de/sollder1/stuff/Main.java
+++ src/main/java/de/sollder1/stuff/Main.java
@@ -7,7 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Tokenizer tokenizer = new Tokenizer("null true");
+        String testJson = """
+                [{"id":"53590461-f322-4777-a724-299be1225a1f","createdBy":null,"lastUpdatedBy"
+                :null,"createdAt":1641158586106,"lastUpdatedAt":1641158586106,"name":"Investment"},
+                {"id":"d22f39dc-9fbe-4c60-992a-281302922371","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641158619839,"lastUpdatedAt":1641158619839,"name":"Wohnen"},
+                {"id":"663217ef-3097-4715-8c93-55238e1f802a","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641158703359,"lastUpdatedAt":1641158703359,"name":"Abos"},
+                {"id":"40e51460-05ef-41af-b0a2-e4eb470af37e","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641159046180,"lastUpdatedAt":1641159046180,"name":"Versicherungen"},
+                {"id":"b1124c8d-b329-4cce-8fd5-d60da725df72","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641203748901,"lastUpdatedAt":1641203748901,"name":"Essen/Trinken"},
+                {"id":"c295d59a-7611-446e-9e36-95e85f4571ce","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641380046223,"lastUpdatedAt":1641380046223,"name":"Telekommunikation"},
+                {"id":"17f9cc99-2e82-4c4e-94dd-bfe2b91300dd","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641668976534,"lastUpdatedAt":1641668976534,"name":"Auto"},
+                {"id":"9386d2ab-899a-4de5-b092-7df4ebc6fbe4","createdBy":null,"lastUpdatedBy":null,
+                "createdAt":1641159252436,"lastUpdatedAt":1641159252436,"name":"Gehalt"}]
+                """;
+
+        Tokenizer tokenizer = new Tokenizer(testJson);
         tokenizer.tokenize();
         tokenizer.print();
 
--- src/main/java/de/sollder1/stuff/jsonparser/tokenizer/Tokenizer.java
+++ src/main/java/de/sollder1/stuff/jsonparser/tokenizer/Tokenizer.java
@@ -53,6 +53,9 @@ public class Tokenizer {
             case ':':
                 tokens.add(new Token(TokenType.COLON));
                 return true;
+            case ',':
+                tokens.add(new Token(TokenType.COMMA));
+                return true;
             case '[':
                 tokens.add(new Token(TokenType.SQUARE_BRACKET_START));
                 return true;
@@ -90,14 +93,81 @@ public class Tokenizer {
 
     private boolean findNumberLiteral() {
 
-        return false;
+        StringBuilder literal = new StringBuilder();
+        boolean negative = false;
+
+        if (inputString.charAt(index) == '-') {
+            literal.append('-');
+            negative = true;
+            index++;
+        }
+
+        if (!isNumeric(inputString.charAt(index))) {
+            if (negative) {
+                throw TokenizerException.create(inputString, index);
+            } else {
+                return false;
+            }
+        }
+
+        boolean colonSet = false;
+        boolean exponentSet = false;
+
+        while (true) {
+            var charAt = inputString.charAt(index);
+            if (isNumeric(charAt)) {
+                //nothing happens here...
+            } else if (charAt == '.') {
+                if (colonSet) {
+                    throw TokenizerException.create(inputString, index);
+                } else {
+                    colonSet = true;
+                }
+            } else if (charAt == 'e' || charAt == 'E') {
+                if (exponentSet) {
+                    throw TokenizerException.create(inputString, index);
+                } else {
+                    exponentSet = true;
+                }
+            } else {
+                break;
+            }
+            literal.append(charAt);
+            index++;
+        }
+
+        tokens.add(new Token(TokenType.NUMBER_LITERAL, literal.toString()));
+
+        return true;
     }
 
     private boolean findStringLiteral() {
+        StringBuilder literal = new StringBuilder();
 
-        return false;
+        if (inputString.charAt(index) != '"') {
+            return false;
+        } else {
+            index++;
+        }
+
+        while (true) {
+            var charAt = inputString.charAt(index);
+            if (charAt == '"') {
+                index++;
+                break;
+            }
+            literal.append(charAt);
+            index++;
+        }
+
+        tokens.add(new Token(TokenType.STRING_LITERAL, literal.toString()));
+
+        return true;
     }
 
+    private static boolean isNumeric(char charAt) {
+        return (charAt >= '0' && charAt <= '9');
+    }
 
     public void print() {
         this.tokens.forEach(System.out::println);
